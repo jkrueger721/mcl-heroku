21a3b2a541b9c1ca63d6874751c648e8633e1aa7
jkrueger721 <johnathankrueger@me.com>
2018-04-20 16:42:00 -0400

    service update

---
+++ Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using MusiCoLab2.Models;
+
+namespace MusiCoLab2.Services
+{
+    public interface IUserService
+    {
+        void AddProjectUser(ProjectUser projectuser);
+        void AddUser(User user);
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using MusiCoLab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusiCoLab2.Services
+{
+    public class UserService : IUserService
+    {
+        private UserContext _db;
+        public UserService(UserContext db)
+        {
+            _db = db;
+        }
+        public void AddUser(User user)
+        {
+            _db.Users.Add(user);
+            _db.SaveChanges();
+        }
+        public void AddProjectUser(ProjectUser projectuser)
+        {
+            _db.Add(projectuser);
+            _db.SaveChanges();
+        }
+
+    }
+}
