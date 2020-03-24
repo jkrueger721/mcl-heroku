9e0f86522a6ce41b160654fe4566f502d1c72e1d
Johnathan Krueger <johnathankrueger@unknown3cf86205b685.attlocal.net>
2020-03-23 22:52:03 -0400

    ...

--- Modals/Project.cs
+++ Modals/Project.cs
@@ -8,26 +8,26 @@ using MusiCoLab2.Modals;
 
 namespace MusiCoLab2.Models
 {
-    public interface IProject
+    public class IProject
  
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-         int Id { get; set; }
+        public int Id { get; set; }
 
-         string Name { get; set; }
-         bool IsPrivate { get; set; }
-         string Daw { get; set; }
-         string Style { get; set; }
-         string Instruments { get; set; }
-         string Comments { get; set; }
-         string AudioUrl { get; set; }
+        public string Name { get; set; }
+      public   bool IsPrivate { get; set; }
+       public  string Daw { get; set; }
+        public string Style { get; set; }
+     public    string Instruments { get; set; }
+       public  string Comments { get; set; }
+      public   string AudioUrl { get; set; }
 
         //[InverseProperty("Owner")]
-         User ProjectOwner { get; set; }
+      public   User ProjectOwner { get; set; }
 
         //[InverseProperty("Contributor")]
-         List<ProjectContributor> ProjectContributors { get; set; }
+       public  List<ProjectContributor> ProjectContributors { get; set; }
 
 
     }
